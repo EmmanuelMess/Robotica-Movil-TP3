cd503ce3bad1712bd1487031eaaab529c511ce88
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-10-06 17:03:17 -0300

    Agregar todo a la ejecucion

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 ## Setup
-1)En una terminal primero corremos la simulacion
+### 1) En una terminal levantamos el sistema
+```bash
+bash dev/setup.sh
+docker compose build
+docker compose -d up
+```
+
+### 2) Levantamos la simulacion
 ```bash
 docker exec -it simulation_env bash
 source /opt/ros/humble/setup.bash
 . /usr/share/gazebo/setup.bash
-export TURTLEBOT3_MODEL=waffle
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/humble/share/turtlebot3_gazebo/models
-ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False
+ros2 launch stereo_image_proc stereo_image_proc.launch.py
+```
+
+### 3) Levantar el rosbag
+```bash
+docker exec -it simulation_env bash
+source /opt/ros/humble/setup.bash
+. /usr/share/gazebo/setup.bash
+ros2 bag play /catkin_ws/src/rosbag2_2022_11_09-15_21_22/  --loop --remap /stereo/left/image_raw:=/left/image_raw /stereo/left/camera_info:=/left/camera_info /stereo/right/image_raw:=/right/image_raw /stereo/right/camera_info:=/right/camera_info
+```
+
+### 4) Levantar el nodo nuestro
+```bash
+docker exec -it simulation_env bash
+source /opt/ros/humble/setup.bash
+. /usr/share/gazebo/setup.bash
+python3 /catkin_ws/src/image_server.py
 ```
--- src/image_server.py
+++ src/image_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import rclpy
 from rclpy.node import Node
 from visualization_msgs.msg import MarkerArray
@@ -17,9 +18,9 @@ from cv_bridge import CvBridge
 import cv2 
 from matplotlib import pyplot as plt
 
-#Correr el punto del: ros2 launch stereo_image_proc stereo_image_proc.launch.py
-#Correr el rosbag
-#Correr este con python3 XXXXX.py
+import numpy as np
+
+RESULT_PATH = "/catkin_ws/src/result/"
 
 class ImageServer(Node):
     
@@ -30,10 +31,34 @@ class ImageServer(Node):
         self.stereo = cv2.StereoBM_create(numDisparities = 16, blockSize = 15)
         self.projectionMatrixLeft = [0,0,0] #Supongo que de la calibracion viene esto nvm
         self.projectionMatrixRight = [0,0,0]
-    
-    def extract_SIFT(frame_msg):
+
+        self.syncImages()
+
+    def syncImages(self):
+        self.left_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/left/image_rect"))
+        self.right_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/right/image_rect"))
+        ts = message_filters.TimeSynchronizer([self.left_rect, self.right_rect], 10)
+        ts.registerCallback(self.callback)
+
+    def callback(self, left_msg, right_msg):
+        self.get_logger().info('Receiving synced video frame')
+
+        frame_SIFT_left, kpntL, descrL = self.extract_SIFT(left_msg)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_left_sift.jpg'), current_frame_left_sift)
+
+        frame_SIFT_right, kpntR, descrR = self.extract_SIFT(right_msg)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_right_sift.jpg'), frame_SIFT_right)
+
+        good_matches = self.feature_matching(frame_cv_left, kpntL, descrL, frame_cv_right, kpntR, descrR)
+        pts = self.triangulacion(kpntL, kpntR, matches)
+        esencialMatrix = self.findHomograficaOEsencial(kpntL, kpntR, matches)
+        current_frame_L = self.bridge.imgmsg_to_cv2(left_msg)
+        current_frame_R = self.bridge.imgmsg_to_cv2(right_msg)
+        disparityMap = self.stereo_matches_disparity(current_frame_L, current_frame_R)
+
+    def extract_SIFT(self, frame_msg):
         current_frame = self.bridge.imgmsg_to_cv2(frame_msg)
-        cv2.imshow("camera", current_frame)  
+        cv2.imwrite(os.path.join(RESULT_PATH, 'sift_frame.jpg'), current_frame)
       
         # Converting image to grayscale
         gray= cv2.cvtColor(current_frame,cv2.COLOR_BGR2GRAY)
@@ -49,23 +74,21 @@ class ImageServer(Node):
                                      flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         return (frame_sift,kp, descr)
         
-    def feature_matching(frame_cv_left, kpntL, descrL,frame_cv_right,kpntR, descrR):
+    def feature_matching(self, frame_cv_left, kpntL, descrL,frame_cv_right,kpntR, descrR):
         matches = bf.knnMatch(descrL,descrR,k=2)
         matches_image = cv2.drawMatchesKnn(frame_cv_left,kpntL,frame_cv_right,kpntR,matches, None,
                                            matchColor=(255,0,0), matchesMask=None,
                                            singlePointColor=(0,255,0), flags=0)
-        cv2.imshow("Matches L and R", matches_image)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'matches.jpg'), matches_image)
         
         ratio_thresh = 0.9
         good_matches = []
         for m,n in matches:
             if m.distance < ratio_thresh * n.distance:
                 good_matches.append(m)
-        
-        cv2.waitKey(0)
         return good_matches
     
-    def triangulacion(kpntL,kpntR, matches):
+    def triangulacion(self, kpntL,kpntR, matches):
         ptsL = np.float32([kpntL[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         ptsR = np.float32([kpntR[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
         
@@ -76,51 +99,17 @@ class ImageServer(Node):
         pts = triagulitos.T[:,:3]
         return pts
         
-    def findHomograficaOEsencial(kpntL,kpntR, matches):
+    def findHomograficaOEsencial(self, kpntL,kpntR, matches):
         ptsL = np.float32([kpntL[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
         ptsR = np.float32([kpntR[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
         
         esencial, _ = cv2.findHomography(ptsL,ptsR, cv2.RANSAC, ransacReprojThreshold = 2.0)
         return esencial
         
-    def stereo_matches_disparity(frame_L,frame_R):
+    def stereo_matches_disparity(self, frameL,frameR):
         disparity = self.stereo.compute(frameL,frameR)
-        plt.imshow(disparity, 'gray')
-        plt.show()
+        cv2.imwrite(os.path.join(RESULT_PATH, 'gray.jpg'), disparity)
         return disparity
-    
-    def a(self):
-        # Creamos subscribers a las cámaras izq y der
-        self.left_rect = message_filters.Subscriber(self, Image, "/left/image_rect")
-        self.right_rect = message_filters.Subscriber(self, Image, "/right/image_rect")
-        # Creamos el objeto ts del tipo TimeSynchronizer encargado de sincronizar los mensajes recibidos.
-        ts = message_filters.TimeSynchronizer([self.left_rect, self.right_rect], 10)
-        # Registramos un callback para procesar los mensajes sincronizados.
-        ts.registerCallback(self.callback)
-
-    def callback(self, left_msg, right_msg):
-        self.get_logger().info('Receiving video frame')
-        ##############
-        frame_SIFT_left, kpntL, descrL = self.extract_SIFT(left_msg)
-        cv2.imshow("cameraLeft", frame_SIFT_left)
-        cv2.imwrite('current_frame_left_sift.jpg', current_frame_left_sift)
-        
-        frame_SIFT_right, kpntR, descrR = self.extract_SIFT(right_msg)
-        cv2.imshow("cameraRight", frame_SIFT_right)
-        cv2.imwrite('current_frame_right_sift.jpg', frame_SIFT_right)
-        ##############
-        good_matches = self.feature_matching(frame_cv_left, kpntL, descrL,frame_cv_right,kpntR, descrR)
-        ##############
-        pts = self.triangulacion(kpntL,kpntR, matches)
-        ##############
-        esencialMatrix = self.findHomograficaOEsencial(kpntL,kpntR, matches)
-        ##############
-        current_frame_L = self.bridge.imgmsg_to_cv2(left_msg)
-        current_frame_R = self.bridge.imgmsg_to_cv2(right_msg)
-        disparityMap = self.stereo_matches_disparity(current_frame_L,current_frame_R)
-        ##############
-        cv2.waitKey(1)
-        pass
 
 def main(args=sys.argv):
     rclpy.init(args=args)
