534bd93c049bc974c964e39d4a885632839b8c23
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-10-08 11:57:26 -0300

    Arreglados algunos problemas de SIFT

--- README.md
+++ README.md
@@ -3,29 +3,23 @@
 ```bash
 bash dev/setup.sh
 docker compose build
-docker compose -d up
+docker compose up -d
 ```
 
 ### 2) Levantamos la simulacion
 ```bash
 docker exec -it simulation_env bash
-source /opt/ros/humble/setup.bash
-. /usr/share/gazebo/setup.bash
-ros2 launch stereo_image_proc stereo_image_proc.launch.py
+source /opt/ros/humble/setup.bash && . /usr/share/gazebo/setup.bash && ros2 launch stereo_image_proc stereo_image_proc.launch.py
 ```
 
 ### 3) Levantar el rosbag
 ```bash
 docker exec -it simulation_env bash
-source /opt/ros/humble/setup.bash
-. /usr/share/gazebo/setup.bash
-ros2 bag play /catkin_ws/src/rosbag2_2022_11_09-15_21_22/  --loop --remap /stereo/left/image_raw:=/left/image_raw /stereo/left/camera_info:=/left/camera_info /stereo/right/image_raw:=/right/image_raw /stereo/right/camera_info:=/right/camera_info
+source /opt/ros/humble/setup.bash && . /usr/share/gazebo/setup.bash && ros2 bag play /catkin_ws/src/rosbag2_2022_11_09-15_21_22/  --loop --remap /stereo/left/image_raw:=/left/image_raw /stereo/left/camera_info:=/left/camera_info /stereo/right/image_raw:=/right/image_raw /stereo/right/camera_info:=/right/camera_info
 ```
 
 ### 4) Levantar el nodo nuestro
 ```bash
 docker exec -it simulation_env bash
-source /opt/ros/humble/setup.bash
-. /usr/share/gazebo/setup.bash
-python3 /catkin_ws/src/image_server.py
+source /opt/ros/humble/setup.bash && . /usr/share/gazebo/setup.bash && python3 /catkin_ws/src/image_server.py
 ```
--- src/image_server.py
+++ src/image_server.py
@@ -27,7 +27,7 @@ class ImageServer(Node):
     def __init__(self):
         super().__init__('publish_cylinders_server')
         self.bridge = CvBridge()
-        self.bf = cv2.BFMatcher()
+        self.bfMatcher = cv2.BFMatcher()
         self.stereo = cv2.StereoBM_create(numDisparities = 16, blockSize = 15)
         self.projectionMatrixLeft = [0,0,0] #Supongo que de la calibracion viene esto nvm
         self.projectionMatrixRight = [0,0,0]
@@ -35,50 +35,43 @@ class ImageServer(Node):
         self.syncImages()
 
     def syncImages(self):
-        self.left_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/left/image_rect"))
-        self.right_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/right/image_rect"))
-        ts = message_filters.TimeSynchronizer([self.left_rect, self.right_rect], 10)
+        left_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/left/image_rect"))
+        right_rect = message_filters.Subscriber(self, Image, os.path.join(RESULT_PATH, "/right/image_rect"))
+        ts = message_filters.TimeSynchronizer([left_rect, right_rect], 10)
         ts.registerCallback(self.callback)
 
     def callback(self, left_msg, right_msg):
         self.get_logger().info('Receiving synced video frame')
 
-        frame_SIFT_left, kpntL, descrL = self.extract_SIFT(left_msg)
-        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_left_sift.jpg'), current_frame_left_sift)
+        frameSiftLeft, keypointsLeft, descriptorLeft = self.extract_SIFT(left_msg)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_left_sift.jpg'), frameSiftLeft)
 
-        frame_SIFT_right, kpntR, descrR = self.extract_SIFT(right_msg)
-        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_right_sift.jpg'), frame_SIFT_right)
+        frameSiftRight, keypointsRight, descriptorRight = self.extract_SIFT(right_msg)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'current_frame_right_sift.jpg'), frameSiftRight)
 
-        good_matches = self.feature_matching(frame_cv_left, kpntL, descrL, frame_cv_right, kpntR, descrR)
-        pts = self.triangulacion(kpntL, kpntR, matches)
-        esencialMatrix = self.findHomograficaOEsencial(kpntL, kpntR, matches)
-        current_frame_L = self.bridge.imgmsg_to_cv2(left_msg)
-        current_frame_R = self.bridge.imgmsg_to_cv2(right_msg)
-        disparityMap = self.stereo_matches_disparity(current_frame_L, current_frame_R)
+        good_matches = self.feature_matching(frameSiftLeft, keypointsLeft, descriptorLeft, frameSiftRight,
+                                             keypointsRight, descriptorRight)
+        normalizedPoints = self.triangulation(keypointsLeft, keypointsRight, good_matches)
+        esencialMatrix = self.findHomograficaOEsencial(keypointsLeft, keypointsRight, good_matches)
+        currentFrameLeft = self.bridge.imgmsg_to_cv2(left_msg)
+        currentFrameRight = self.bridge.imgmsg_to_cv2(right_msg)
+        disparityMap = self.stereoMatchesDisparity(currentFrameLeft, currentFrameRight)
 
     def extract_SIFT(self, frame_msg):
-        current_frame = self.bridge.imgmsg_to_cv2(frame_msg)
-        cv2.imwrite(os.path.join(RESULT_PATH, 'sift_frame.jpg'), current_frame)
-      
-        # Converting image to grayscale
-        gray= cv2.cvtColor(current_frame,cv2.COLOR_BGR2GRAY)
+        currentFrame = self.bridge.imgmsg_to_cv2(frame_msg)
+        cv2.imwrite(os.path.join(RESULT_PATH, 'sift_frame.jpg'), currentFrame)
          
-        # Applying SIFT detector
-        sift = cv2.xfreatures2d.SIFT_create()
-        kp, descr = sift.detectAndCompute(current_frame, None) #Son los keypoint
+        sift = cv2.SIFT_create()
+        keyPoints, descriptors = sift.detectAndCompute(currentFrame, None)
          
-        # Marking the keypoint on the image using circles
-        frame_sift=cv2.drawKeypoints(gray,
-                                     kp,
-                                     current_frame,
+        frame_sift=cv2.drawKeypoints(currentFrame, keyPoints, currentFrame, 
                                      flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        return (frame_sift,kp, descr)
+        return frame_sift, keyPoints, descriptors
         
-    def feature_matching(self, frame_cv_left, kpntL, descrL,frame_cv_right,kpntR, descrR):
-        matches = bf.knnMatch(descrL,descrR,k=2)
-        matches_image = cv2.drawMatchesKnn(frame_cv_left,kpntL,frame_cv_right,kpntR,matches, None,
-                                           matchColor=(255,0,0), matchesMask=None,
-                                           singlePointColor=(0,255,0), flags=0)
+    def feature_matching(self, frameCvLeft, keypointsLeft, descriptorLeft, frameCvRight, keypointsRight, descriptorRight):
+        matches = self.bfMatcher.knnMatch(descriptorLeft, descriptorRight, k=2)
+        matches_image = cv2.drawMatchesKnn(frameCvLeft, keypointsLeft, frameCvRight, keypointsRight, matches, None,
+                                           matchColor=(255,0,0), matchesMask=None, singlePointColor=(0,255,0), flags=0)
         cv2.imwrite(os.path.join(RESULT_PATH, 'matches.jpg'), matches_image)
         
         ratio_thresh = 0.9
@@ -88,25 +81,24 @@ class ImageServer(Node):
                 good_matches.append(m)
         return good_matches
     
-    def triangulacion(self, kpntL,kpntR, matches):
-        ptsL = np.float32([kpntL[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-        ptsR = np.float32([kpntR[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    def triangulation(self, keypointsLeft, keypointsRight, matches):
+        pointsLeft = np.float32([keypointsLeft[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        pointsRight = np.float32([keypointsRight[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
         
-        triagulitos = cv2.triangulatePoint(self.projectionMatrixLeft,self.projectionMatrixRight,
-                                           ptsL,ptsR)
-        #Messu publica pts como un PointCLoud2 asi va a RVIZ2
-        triagulitos /= triagulitos[3]
-        pts = triagulitos.T[:,:3]
-        return pts
+        triangulatedPoints = cv2.triangulatePoints(self.projectionMatrixLeft, self.projectionMatrixRight, pointsLeft, pointsRight)
+        # TODO Messu publica pts como un PointCLoud2 asi va a RVIZ2
+        triangulatedPoints /= triangulatedPoints[3]
+        normalizedPoints = triangulatedPoints.T[:,:3]
+        return normalizedPoints
         
     def findHomograficaOEsencial(self, kpntL,kpntR, matches):
-        ptsL = np.float32([kpntL[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
-        ptsR = np.float32([kpntR[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+        pointsLeft = np.float32([kpntL[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        pointsRight = np.float32([kpntR[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
         
-        esencial, _ = cv2.findHomography(ptsL,ptsR, cv2.RANSAC, ransacReprojThreshold = 2.0)
+        esencial, _ = cv2.findHomography(pointsLeft, pointsRight, cv2.RANSAC, ransacReprojThreshold = 2.0)
         return esencial
         
-    def stereo_matches_disparity(self, frameL,frameR):
+    def stereoMatchesDisparity(self, frameL, frameR):
         disparity = self.stereo.compute(frameL,frameR)
         cv2.imwrite(os.path.join(RESULT_PATH, 'gray.jpg'), disparity)
         return disparity
